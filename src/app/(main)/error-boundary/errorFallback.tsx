0b335e50b21a7b733c61dc72dbcd7f9a3a89d63c
Ryosuke1208 <miyuki.ryosuke3394@gmail.com>
2024-08-14 21:59:11 +0900

    error-boudar修正

--- src/app/(main)/error-boundary/errorFallback.tsx
+++ src/app/(main)/error-boundary/errorFallback.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { FallbackProps } from "react-error-boundary";
 
 const ErrorFallback = () => {
   return (
--- src/app/(main)/error-boundary/page.tsx
+++ src/app/(main)/error-boundary/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { ErrorBoundary, useErrorBoundary } from "react-error-boundary";
+import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./errorFallback";
-import Button from "@mui/material/Button/Button";
-import { restUtils } from "./restUtils";
 import FetchButton from "./fetchButton";
 
 const Page = () => {
